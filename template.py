d0925464d27ec15411c0f5fa042667b0b82927d4
mohammadshuaib07866 <mohammadshuaib07866@gmail.com>
2025-11-14 13:38:50 +0530

    Project Structured Completed

--- template.py
+++ template.py
@@ -4,8 +4,7 @@ from pathlib import Path
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s]: %(levelname)s: %(message)s"
+    level=logging.INFO, format="[%(asctime)s]: %(levelname)s: %(message)s"
 )
 
 list_files = [
@@ -15,10 +14,11 @@ list_files = [
     "src/components/data_ingestion.py",
     "src/components/data_transformation.py",
     "src/components/model_trainer.py",
-    "src/model_evaluation.py",
+    "src/components/model_evaluation.py",
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
+    "src/commons/__init__.py",
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/__init__.py",
@@ -33,6 +33,7 @@ list_files = [
     "setup.cfg",
     "pyproject.toml",
     "tox.ini",
+    ".env"
     "experiment/experiments.ipynb",
 ]
 
